ce52e390b8b890eb92ff76b1b0be3c754de4800f
David Pérez <davidpe1307@gmail.com>
2026-04-27 18:26:46 -0500

    Agrego clase david

---
+++ src/calculadoraap/David.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadoraap;
+
+/**
+ *
+ * @author Usuario
+ */
+public class David {
+    public static void main(String[] args){
+        System.out.println("mi nombre es David");
+    }
+}
